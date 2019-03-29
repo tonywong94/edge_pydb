27ab0eb3abaaf2fb2cedada68a04d20fe3111920
Tony Wong <tonywong94@gmail.com>
2019-03-29 10:56:23 -0500

    fixes for flux_elines

--- img_califa/build/do_califa.py
+++ img_califa/build/do_califa.py
@@ -4,7 +4,6 @@
 
 import glob
 import os
-import numpy as np
 from astropy.table import Table, Column, join, vstack
 import numpy as np
 from astropy.io import fits
@@ -42,26 +41,32 @@ for prod in prodtype:
         for key in ['NAXIS1', 'NAXIS2', 'CTYPE1', 'CTYPE2', 'CRVAL1', 'CRVAL2', 
                     'CRPIX1', 'CRPIX2', 'CDELT1', 'CDELT2']:
             outhd[key] = cohd[key]
-        for key in ['CD1_1','CD1_2','CD2_1','CD2_2']:
-            del outhd[key]
+        cdkeys = ['CD1_1','CD1_2','CD2_1','CD2_2','CD1_3','CD2_3','CD3_1','CD3_2','CD3_3']
+        for key in cdkeys:
+            if key in outhd.keys():
+                del outhd[key]
+        ##for key in ['CD1_3','CD2_3','CD3_1','CD3_2','CD3_3']:
+         #   del outhd[key]
         #print(repr(outhd))
 
         # First process the native resolution file since it has the astrometry
         hdu = fits.open(cadir+'x'+base, ignore_missing_end=True)[0]
         hdu.data[hdu.data==0] = np.nan
         newim,foot = reproject_interp(hdu, outhd, independent_celestial_slices=True)
-        #fits.writeto('regridded.fits.gz', newim, outhd)
+        fits.writeto(base.replace('fits','rgd.fits'), newim, outhd, overwrite=True)
         tab0 = fitsextract(newim, header=outhd, keepnan=True, stride=[3,3,1], 
             bunit=units, lbl=labels, zselect=zsel, suffix='_rg')
+        print('There are',len(tab0),'rows')
 
         # Then process the smoothed file
         hdu = fits.open(cadir+base, ignore_missing_end=True)[0]
         hdu.data[hdu.data==0] = np.nan
         hdu.header = cahd
         newim,foot = reproject_interp(hdu, outhd, independent_celestial_slices=True)
-        #fits.writeto('smoothed.fits.gz', newim, outhd)
+        fits.writeto(base.replace('fits','sm.fits'), newim, outhd, overwrite=True)
         tab1 = fitsextract(newim, header=outhd, keepnan=True, stride=[3,3,1], 
             bunit=units, lbl=labels, zselect=zsel, suffix='_sm')
+        print('There are',len(tab1),'rows')
 
         joint=join(tab0,tab1)
 
@@ -76,6 +81,7 @@ for prod in prodtype:
     if len(tablelist) > 0:
         t_merge = vstack(tablelist)
     print(t_merge.colnames)
+    print('There are',len(t_merge),'rows')
     if (len(filelist) > 1):
         outname = 'edge'
     else:
