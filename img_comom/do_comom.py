e3d7242a71ed146b1a0bb40d54a65dc4e91ab656
Tony Wong <tonywong94@gmail.com>
2019-04-03 16:24:43 -0500

    Update to images

--- img_comom/do_comom.py
+++ img_comom/do_comom.py
@@ -9,35 +9,38 @@ import glob
 import os
 import numpy as np
 from astropy.table import Table, Column, join, vstack
-#from astropy.wcs import WCS
 
 msktyp = ['dil', 'smo']
 
 for i, msk in enumerate(msktyp):
-    filelist = glob.glob('fitsdata/*.co.smo7_'+msk+'.mom0.fits.gz')
+    filelist = glob.glob('fitsdata/*.co.smo7_'+msk+'.emom0max.fits.gz')
     tablelist=[]
     for file in filelist:
-    	# Read the mom0 image
+    	# Read the emom0max image first (available for all galaxies)
         gal = os.path.basename(file).split('.')[0]
-        tab0 = fitsextract(file, bunit='K km/s', col_name='mom0',
+        tab0 = fitsextract(file, bunit='K km/s', col_lbl='emom0max',
         					keepnan=True, stride=[3,3,1])
         gname = Column([np.string_(gal)]*len(tab0), name='Name', description='Galaxy Name')
         tab0.add_column(gname, index=0)
         print(tab0[20:50])
     	# Read the other images
         if msk == 'smo':
-            dotypes = ['emom0', 'emom0max']
+            dotypes = ['mom0', 'emom0']
             unit    = ['K km/s', 'K km/s']
         else:
-            dotypes = ['emom0', 'emom0max', 'mom1', 'emom1', 'mom2', 'emom2', 'snrpk']
+            dotypes = ['mom0', 'emom0', 'mom1', 'emom1', 'mom2', 'emom2', 'snrpk']
             unit    = ['K km/s', 'K km/s', 'km/s', 'km/s', 'km/s', 'km/s', '']
         for j, type in enumerate(dotypes):
-            addtb = fitsextract('fitsdata/'+gal+'.co.smo7_'+msk+'.'+type+'.fits.gz', 
-                                bunit=unit[j], col_name=type, keepnan=True, 
-                                stride=[3,3,1])
-            #addtb['imgdata'].name = type
-            jointb = join(tab0, addtb)
-            tab0 = jointb
+            getfile = 'fitsdata/'+gal+'.co.smo7_'+msk+'.'+type+'.fits.gz'
+            if os.path.exists(getfile):
+                addtb = fitsextract(getfile, bunit=unit[j], col_lbl=type, 
+                                keepnan=True, stride=[3,3,1])
+                jointb = join(tab0, addtb)
+                tab0 = jointb
+            else:
+                newcol = Column(data=[np.nan]*len(tab0), name=type, 
+                                unit=unit[j], dtype='f4')
+                tab0.add_column(newcol)
         tablelist.append(tab0)
     if len(tablelist) > 0:
         t_merge = vstack(tablelist)
