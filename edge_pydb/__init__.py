8c6e6b633d1b818de8777914288b7df34e3055bb
Brook Luo <brookluo1997@gmail.com>
2019-07-29 14:09:45 -0500

    EdgeTable class update

--- edge_pydb/__init__.py
+++ edge_pydb/__init__.py
@@ -3,12 +3,19 @@ from astropy.table import Table as _Table
 from astropy.table import join as _join
 
 
-class EdgeTable:
-    def __init__(self, file, path):
-        self.table = _Table.read(util.fetch(file), path)
-        self.path = path
+class EdgeTable(_Table):
+    def __init__(self, file='', path=''):
+        super().__init__()
+        if file and path:
+            self.read(file, path)
         self.srcfile = file
-
+        self.path = path
+        self.joined = []
+        
+    def read(self, file, path):
+        self.table = _Table.read(util.fetch(file), path=path)
+        self.__dict__.update(self.table.__dict__)
+        
     def join(self, file, join_type='inner', path=''):
         if 'csv' in file:
             target = _Table.read(util.fetch(file), format='ascii.ecsv')
@@ -18,4 +25,6 @@ class EdgeTable:
                 path = self.path
             target = _Table.read(util.fetch(file), path)
         self.table = _join(self.table, target, join_type=join_type, keys='Name')
-        
+        # update the data
+        self.__dict__.update(self.table.__dict__)
+        self.joined.append((file, path, join_type))
