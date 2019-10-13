2b9f046989fa8f58e6ac8e496d95719ef5f9f180
Tony Wong <tonywong94@gmail.com>
2019-10-13 16:26:18 -0500

    Revert to older LEDA values, update Azplot.ipynb, update edge_rfpars.csv

--- edge_pydb/dat_glob/derived/build/becca2ecsv.py
+++ edge_pydb/dat_glob/derived/build/becca2ecsv.py
@@ -4,7 +4,9 @@ from astropy.table import Table
 import numpy as np
 from datetime import datetime
 
-infile = "EDGE_COparameters_20180604.csv"
+infile = "EDGE_COparameters_20191004.csv"
+head_start = 21  # previously 18
+dat_start = 22   # previously 19
 
 # Get header names from top of file
 headnames = {}
@@ -12,19 +14,20 @@ with open(infile) as f:
     for line in f:
         if ":" in line:
             str=line.split(':',1)
-            headnames[str[0]] = str[1].strip()
+            headnames[str[0]] = str[1].strip().strip(",")
 
+print('Information found for {} columns'.format(len(headnames)))
 for key in headnames:
     print(key)
 
 # Get data, add 'rf' string before column names
-t = Table.read(infile, format='ascii.csv', header_start=18, data_start=19)
+t = Table.read(infile, format='ascii.csv', header_start=head_start, data_start=dat_start)
 t.pprint()
 for cname in t.colnames:
     t[cname].description = headnames[cname]
-    #t[cname].name = t[cname].name
 
 t['rfVsys'].unit = 'km / s'
+t['rfVsys'] = t['rfVsys'].astype(float)
 t['rfLSRK2helio'].unit = 'km / s'
 t['rfRflat'].unit = 'arcsec'
 t['rfVrotMaxL'].unit = 'km / s'
@@ -37,6 +40,10 @@ t['rfKinXoff'].unit = 'arcsec'
 t['rfKinYoff'].unit = 'arcsec'
 t['rfKinRA'].unit = 'hourangle'
 t['rfKinDecl'].unit = 'deg'
+
+for extracol in ['ledaRA','ledaDE']:
+    if extracol in t.colnames:
+        t.remove_column(extracol)
 
 t.meta['date'] = datetime.today().strftime('%Y-%m-%d')
 print(t.meta)
