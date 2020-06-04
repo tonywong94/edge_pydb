f0804036bb739e0be96e7088a4795c25ad7cd2df
Tony Wong <tonywong94@gmail.com>
2020-06-03 22:16:48 -0500

    Regenerate HDF5 files

--- edge_pydb/img_comom/do_comom.py
+++ edge_pydb/img_comom/do_comom.py
@@ -15,23 +15,24 @@ msktyp = ['dil', 'smo']
 lines  = ['12', '13']
 
 # Get the orientation parameters from LEDA
-ort = EdgeTable('edge_leda.csv', cols=['Name', 'ledaRA', 'ledaDE', 'ledaPA', 'ledaIncl'])
-#ort = EdgeTable('edge_rfpars.csv', cols=['Name', 'rfPA', 'rfInc', 'rfKinRA', 'rfKinDecl'])
+ort = EdgeTable('edge_leda.csv', cols=['Name', 'ledaRA', 'ledaDE', 'ledaPA', 'ledaAxIncl'])
 ort.add_index('Name')
 
 for i, msk in enumerate(msktyp):
     gallist = [os.path.basename(file).split('.')[0] for file in 
-                sorted(glob.glob('fitsdata/*.co.smo7_'+msk+'.emom0max.fits.gz'))] 
+                sorted(glob.glob('fitsdata/*.co.smo7_'+msk+'.snrpk.fits.gz'))] 
     tablelist=[]
     for gal in gallist:
-        file0 = 'fitsdata/'+gal+'.co.smo7_'+msk+'.emom0max.fits.gz'
+        file0 = 'fitsdata/'+gal+'.co.smo7_'+msk+'.snrpk.fits.gz'
         print('Reading',file0)
-        galtab = fitsextract(file0, bunit='K km/s', col_lbl='emom0max_12',
+        adopt_incl = ort.loc[gal]['ledaAxIncl']
+        print('Adopted inclination in {} deg'.format(adopt_incl))
+        galtab = fitsextract(file0, bunit='', col_lbl='snrpk_12',
                             keepnan=True, stride=[3,3,1],
                             ra_gc=15*ort.loc[gal]['ledaRA'],
                             dec_gc=ort.loc[gal]['ledaDE'],
                             pa=ort.loc[gal]['ledaPA'],
-                            inc=ort.loc[gal]['ledaIncl'],
+                            inc=adopt_incl,
                             ortlabel='LEDA', first=True)
         gname = Column([np.string_(gal)]*len(galtab), name='Name', description='Galaxy Name')
         galtab.add_column(gname, index=0)
@@ -40,10 +41,10 @@ for i, msk in enumerate(msktyp):
         # Read the other images
         for line in lines:
             if line == '13':
-                file0 = 'fitsdata/'+gal+'.13co.smo7_mk12_'+msk+'.emom0max.fits.gz'
+                file0 = 'fitsdata/'+gal+'.13co.smo7_'+msk+'.snrpk.fits.gz'
                 if os.path.exists(file0):
                     print('Reading',file0)
-                    addtb = fitsextract(file0, bunit='K km/s', col_lbl='emom0max_13', 
+                    addtb = fitsextract(file0, bunit='', col_lbl='snrpk_13', 
                                     keepnan=True, stride=[3,3,1])
                     jointb = join(galtab, addtb, keys=['ix','iy'])
                     galtab = jointb            
@@ -51,15 +52,13 @@ for i, msk in enumerate(msktyp):
                 dotypes = ['mom0', 'emom0']
                 unit    = ['K km/s', 'K km/s']
             else:
-                dotypes = ['mom0', 'emom0', 'mom1', 'emom1', 'mom2', 'emom2', 'snrpk']
-                unit    = ['K km/s', 'K km/s', 'km/s', 'km/s', 'km/s', 'km/s', '']
+                dotypes = ['mom0', 'emom0', 'mom1', 'emom1', 'mom2', 'emom2']
+                unit    = ['K km/s', 'K km/s', 'km/s', 'km/s', 'km/s', 'km/s']
             for j, type in enumerate(dotypes):
                 if line == '12':
                     getfile = 'fitsdata/'+gal+'.co.smo7_'+msk+'.'+type+'.fits.gz'
-                elif type != 'snrpk':
-                    getfile = 'fitsdata/'+gal+'.13co.smo7_mk12_'+msk+'.'+type+'.fits.gz'
                 else:
-                    getfile = 'fitsdata/'+gal+'.13co.smo7_'+msk+'.'+type+'.fits.gz'
+                    getfile = 'fitsdata/'+gal+'.13co.smo7_mk12_'+msk+'.'+type+'.fits.gz'
                 if os.path.exists(getfile):
                     print('Reading',getfile)
                     addtb = fitsextract(getfile, bunit=unit[j], col_lbl=type+'_'+line, 
@@ -70,17 +69,19 @@ for i, msk in enumerate(msktyp):
                     newcol = Column(data=[np.nan]*len(galtab), name=type+'_'+line, 
                                     unit=unit[j], dtype='f4')
                     galtab.add_column(newcol)
-            # Add the H2 column density, not deprojected
+            # Add the H2 column density, with and without deprojection
             if line == '12':
                 sigmol = msd_co(galtab['mom0_12'], name='sigmol')
                 e_sigmol = msd_co(galtab['emom0_12'], name='e_sigmol')
-                galtab.add_columns([sigmol, e_sigmol])
+                sigmol_fo = msd_co(galtab['mom0_12']*np.cos(np.radians(adopt_incl)), name='sigmol_fo')
+                e_sigmol_fo = msd_co(galtab['emom0_12']*np.cos(np.radians(adopt_incl)), name='e_sigmol_fo')
+                galtab.add_columns([sigmol, e_sigmol, sigmol_fo, e_sigmol_fo])
         tablelist.append(galtab)
 
     if len(tablelist) > 0:
         t_merge = vstack(tablelist)
         for line in lines:
-            t_merge['emom0max_'+line].description = line+'CO error in mom0 assuming 200 km/s window'
+            t_merge['snrpk_'+line].description = line+'CO peak signal to noise ratio'
             t_merge['mom0_'+line].description = line+'CO integrated intensity using {} mask'.format(msk)
             t_merge['emom0_'+line].description = line+'CO error in mom0 assuming {} mask'.format(msk)
             if msk == 'dil':
@@ -88,10 +89,11 @@ for i, msk in enumerate(msktyp):
                 t_merge['emom1_'+line].description = line+'CO error in mom1 assuming {} mask'.format(msk)
                 t_merge['mom2_'+line].description = line+'CO intensity wgtd vel disp using {} mask'.format(msk)
                 t_merge['emom2_'+line].description = line+'CO error in mom2 assuming {} mask'.format(msk)
-                t_merge['snrpk_'+line].description = line+'CO peak signal to noise ratio'
             if line == '12':
                 t_merge['sigmol'].description = 'apparent H2+He surf density not deprojected'
                 t_merge['e_sigmol'].description = 'error in sigmol not deprojected'
+                t_merge['sigmol_fo'].description = 'H2+He surf density deprojected to face-on using ledaAxIncl'
+                t_merge['e_sigmol_fo'].description = 'error in sigmol deprojected to face-on'
         t_merge.meta['date'] = datetime.today().strftime('%Y-%m-%d')   
         print(t_merge[20:50])
 
