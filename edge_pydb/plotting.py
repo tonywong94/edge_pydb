5588c228c29e9c10557ea78cc49a322c87813ec4
Tony Wong <tonywong94@gmail.com>
2020-07-20 14:26:30 -0500

    Bbarolo analysis update & xy2binned plotting function

--- edge_pydb/plotting.py
+++ edge_pydb/plotting.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import stats
 from matplotlib.patches import Circle
 from matplotlib.collections import PatchCollection
 
@@ -20,6 +21,34 @@ def xy2hist(xarr, yarr, log=True, bins=[100,100]):
     if log:
         z = np.log10(z)
     return x, y, z, hh, locx, locy
+
+
+# Prepare binned averages from a scatterplot
+# If log=True, averaging is done after taking the log
+def xy2binned(xarr, yarr, log=True, bins=20, range=None, yval='mean'):
+    if log:
+        good = (xarr>0) & (yarr>0)
+        x = np.log10(xarr[good])
+        y = np.log10(yarr[good])
+    else:
+        x = xarr
+        y = yarr
+    if yval == 'median':
+        stat = lambda y: np.nanmedian(y)
+        estat = lambda y: stats.iqr(y, scale='normal', nan_policy='omit')
+    else:
+        stat = lambda y: np.nanmean(y)
+        estat = lambda y: np.nanstd(y)
+    if bins > 0:
+        ymean, xbinedge, _ = stats.binned_statistic(x, y,
+            statistic=stat, bins=bins, range=range)
+        ystd, xbinedge, _  = stats.binned_statistic(x, y,
+            statistic=estat, bins=bins, range=range)
+        xbin = 0.5*(xbinedge[1:]+xbinedge[:-1])
+        return xbin, ymean, ystd
+    else:
+        return [0], [0], [0]
+
 
 # Prepare a patch collection for a dotplot
 def dotpatch(x, y, colors, size=1, vmin=None, vmax=None, cmap='jet'):
