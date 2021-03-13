94b1615d0938f2378ea6e78f9274492cc0c626ae
Tony Wong <tonywong94@gmail.com>
2021-03-12 19:50:47 -0600

    New plotting routines for rectangular sampling
    Former-commit-id: 4632822b6c512c3bb78c4f1a7af2dbb94f62b535

--- edge_pydb/plotting.py
+++ edge_pydb/plotting.py
@@ -1,7 +1,12 @@
+import os
+import datetime
 import numpy as np
 from scipy import stats
 from matplotlib.patches import Circle
 from matplotlib.collections import PatchCollection
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
 
 # Prepare a 2D histogram from a scatterplot
 def xy2hist(xarr, yarr, log=True, bins=[100,100]):
@@ -51,22 +56,147 @@ def xy2binned(xarr, yarr, log=True, bins=20, range=None, yval='mean'):
 
 
 # Prepare a patch collection for a dotplot
-def dotpatch(x, y, colors, size=1, vmin=None, vmax=None, cmap='jet'):
+# def dotpatch(x, y, colors, size=1, vmin=None, vmax=None, cmap='jet'):
+# 
+#     patches = []
+#     for x1, y1 in zip(x,y):
+#         circle = Circle((x1, y1), size)
+#         patches.append(circle)
+# 
+#     p = PatchCollection(patches, cmap=cmap)
+#     p.set_array(np.array(colors))
+#     if vmax is None:
+#         vmax = np.nanmax(colors)
+#     if vmin is None:
+#         vmin = np.nanmin(colors)
+# 
+#     p.set_clim([vmin, vmax])
+#     xymin = np.min([x.min(),y.min()])
+#     xymax = np.max([x.max(),y.max()])
+# 
+#     return p, xymin, xymax
 
+
+# Prepare a patch collection for a dotplot
+def dotpatch(x, y, imval, blank=None, dotsize=1, axes=None, **kwargs):
+    # blank is a boolean array that sets certain pixels to NaN
+    if blank is not None:
+        imval[blank] = np.nan
+    if axes is None:
+        axes = plt.gca()
+    if 'vmin' in kwargs:
+        vmin = kwargs.pop('vmin')
+    else:
+        vmin = np.nanmin(imval)
+    if 'vmax' in kwargs:
+        vmax = kwargs.pop('vmax')
+    else:
+        vmax = np.nanmax(imval)
     patches = []
     for x1, y1 in zip(x,y):
-        circle = Circle((x1, y1), size)
+        circle = Circle((x1, y1), dotsize)
         patches.append(circle)
-
-    p = PatchCollection(patches, cmap=cmap)
-    p.set_array(np.array(colors))
-    if vmax is None:
-        vmax = np.nanmax(colors)
-    if vmin is None:
-        vmin = np.nanmin(colors)
-
+    p = PatchCollection(patches, **kwargs)
+    p.set_array(np.array(imval))
     p.set_clim([vmin, vmax])
-    xymin = np.min([x.min(),y.min()])
-    xymax = np.max([x.max(),y.max()])
+    xminmax = [x.min(), x.max()]
+    yminmax = [y.min(), y.max()]
+    img = axes.add_collection(p)
+    return img, xminmax, yminmax
 
-    return p, xymin, xymax
+
+# Plot a non-dotpatch image
+def imarrayplot(x, y, imval, blank=None, axes=None, **kwargs):
+    if blank is not None:
+        imval[blank] = np.nan
+    if axes is None:
+        axes = plt.gca()
+    xdim = len(np.unique(x))
+    ydim = len(np.unique(y))
+    imarray = np.reshape(imval, [ydim,xdim], order='F')
+    xminmax = [0, xdim-1]
+    yminmax = [0, ydim-1]
+    img = axes.imshow(imarray, origin='lower', **kwargs)
+    return img, xminmax, yminmax
+
+
+# Multi-page plots with all galaxies
+def gridplot(edgetab=None, gallist=None, column='flux_Halpha_rg', 
+            xrange=None, yrange=None, blank=None, plotstyle='image',
+            cmap='jet', nx=7, ny=6, dotsize=1, pdfname=None, **kwargs):
+
+    # Plot all galaxies by default
+    if gallist is None:
+        gallist = list(np.unique(edgetab['Name']))
+
+    pages = int(np.ceil(float(len(gallist)) / (nx*ny)))
+    if pdfname is not None:
+        pp = PdfPages(pdfname)
+
+    for num in range(0,pages):
+        aa = nx*ny*num
+        bb = nx*ny+aa
+        thispage = gallist[aa:bb]
+        fig = plt.figure(figsize=(20,14))
+        print("Plotting", thispage)
+
+        for i in range(0,len(thispage)):
+            gname = thispage[i]
+            ax = plt.subplot(ny,nx,i+1)
+            galtab = edgetab['Name'] == gname
+
+            if plotstyle == 'dot':
+                if blank is not None:
+                    img, xran, yran = dotpatch(edgetab[galtab]['ix'], 
+                                               edgetab[galtab]['iy'],
+                                               edgetab[galtab][column], 
+                                               blank=blank[galtab], 
+                                               dotsize=dotsize, cmap=cmap, 
+                                               axes=ax, **kwargs)
+                else:
+                    img, xran, yran = dotpatch(edgetab[galtab]['ix'], 
+                                               edgetab[galtab]['iy'],
+                                               edgetab[galtab][column],
+                                               dotsize=dotsize, cmap=cmap, 
+                                               axes=ax, **kwargs)
+            else:
+                if blank is not None:
+                    img, xran, yran = imarrayplot(edgetab[galtab]['ix'], 
+                                                  edgetab[galtab]['iy'],
+                                                  edgetab[galtab][column], 
+                                                  blank=blank[galtab], 
+                                                  cmap=cmap, axes=ax, **kwargs)        
+                else:
+                    img, xran, yran = imarrayplot(edgetab[galtab]['ix'], 
+                                                  edgetab[galtab]['iy'],
+                                                  edgetab[galtab][column],
+                                                  cmap=cmap, axes=ax, **kwargs)        
+
+            if xrange is None:
+                ax.set_xlim(xran)
+            else:
+                ax.set_xlim(xrange)
+            if yrange is None:
+                ax.set_ylim(yran)
+            else:
+                ax.set_ylim(yrange)
+
+            ax.set_aspect('equal')
+            ax.xaxis.set_ticks([])
+            ax.yaxis.set_ticks([])
+            plt.text(0.04,0.9,gname,ha='left',va='center',transform=ax.transAxes,
+               bbox=dict(facecolor='white', edgecolor='none', pad=1))
+        fig.subplots_adjust(hspace=0.05)
+        fig.subplots_adjust(wspace=0.05)
+        if pdfname is not None:
+            pp.savefig(bbox_inches = 'tight', pad_inches=0.1)
+            plt.close()
+        else:
+            plt.show()
+    if pdfname is not None:
+        d = pp.infodict()
+        d['Title'] = 'EDGE Gallery'
+        d['Author'] = os.getlogin()
+        d['CreationDate'] = datetime.datetime.today()
+        pp.close()
+    return
